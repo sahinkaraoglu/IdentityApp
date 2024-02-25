4e33035e74e8e7a4fd4d4767f2e16b4d19c0fe27
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2024-02-25 20:02:16 +0300

    IdentityContext

--- Models/IdentityContext.cs
+++ Models/IdentityContext.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityApp.Models
 {
-    public class IdentityContext:IdentityDbContext
+    public class IdentityContext:IdentityDbContext<IdentityUser>
     {
-        
+        public IdentityContext(DbContextOptions<IdentityContext> options):base(options)
+        {
+            
+        }
     }
 }
