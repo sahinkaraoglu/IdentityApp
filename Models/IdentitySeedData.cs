b4f2884c4291c913d7e055ea2efcc7226824fca3
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2024-02-26 12:00:50 +0300

    Seed-Data

---
+++ Models/IdentitySeedData.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Admin_123";
+
+        public static async void IdentityTestUser(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+
+            if(context.Database.GetAppliedMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(adminUser);
+
+            if(user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = adminUser,
+                    Email = "admin@sahinkaraoglu.com",
+                    PhoneNumber = "0534000000"
+                };
+
+                await userManager.CreateAsync(user, adminPassword);
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -34,4 +34,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+IdentitySeedData.IdentityTestUser(app);
 app.Run();
